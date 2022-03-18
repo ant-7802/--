2f5dbd3dee1d67716a8eb7a4d2dc4399cf7d7e0d
ANT <68531196+ant-7802@users.noreply.github.com>
2022-03-17 20:55:40 -0400

    Update cframe.lua

--- cframe.lua
+++ cframe.lua
@@ -4,9 +4,9 @@ die = false
     	game.Players.LocalPlayer.Character:WaitForChild("Humanoid").Died:Connect(function()
     	    die = true
     	end)
-	if x == nil then x = 0 end
-	if y == nil then y = 0 end
-	if z == nil then z = 0 end
+if cframenew == nil then
+    cframenew = Cframe.new(0,0,0)
+    end
     game:GetService("RunService").Heartbeat:connect(function() 
             if die == false then
         part.CFrame = parttwo.CFrame * cframenew
